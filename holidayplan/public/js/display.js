870bff5f962e395ebf5b010e58e5cec3e0ca520d
Raluca <raluca_bugnar@yahoo.com>
2017-11-13 12:27:53 +0200

    Update.

--- holidayplan/public/js/display.js
+++ holidayplan/public/js/display.js
@@ -447,11 +447,10 @@ $(document).ready(function() {
             $('.modal-body> div:first-child').css('display', 'block');
             $('.modal-body> div:nth-child(2)').css('display', 'none');
             $('.modal-body> div:nth-child(3)').css('display', 'none');
-<<<<<<< HEAD
-=======
+
 //            resetFormAddH();
 
->>>>>>> delete-holiday-form
+
 
             $("div #info").css('display', 'none');
             $("div #danger").css('display', 'none');
